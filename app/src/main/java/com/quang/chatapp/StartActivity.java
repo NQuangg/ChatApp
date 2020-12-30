1fdbd40f243764fa0916b40edd865d7480db8d0b
ADMIN <ngocquang2k1911@gmail.com>
2020-12-31 00:08:46 +0700

    push

--- app/src/main/java/com/quang/chatapp/StartActivity.java
+++ app/src/main/java/com/quang/chatapp/StartActivity.java
@@ -7,13 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
     private Button login, register;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+//      check if user is null
+        if (firebaseUser != null) {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
